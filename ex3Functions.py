57db3cbf807315a6d7c6a1ca289163eca6e86e72
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2021-06-17 11:38:07 +0300

    conditions

--- ex3Functions.py
+++ ex3Functions.py
@@ -4,3 +4,21 @@ def myFunction(name, age):
     print("My name is " + name + " and my age is " + str(age))
 
 myFunction("Alvis", 27)
+
+
+# print people
+def print_people(*people):
+  for person in people:
+    print("This person is ", person)
+
+print_people("Alvis", "Juris", "Andris")
+
+
+# do basic math
+def do_math(num1, num2):
+  return num1 + num2
+
+math1 = do_math(5, 7)
+math2 = do_math(11, 34)
+
+print("First sum is", math1, "and the secound sum is", math2)
---
+++ ex4Conditions.py
@@ -0,0 +1,10 @@
+# if else elif
+
+check = True
+
+if check == False
+    print("it is false")
+elif check == "It desnt matter any more"
+    print("Something like that")
+else:
+    print("In the end its True")
