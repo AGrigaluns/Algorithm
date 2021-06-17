cf2791b6a5915658c03e447729e71d1ffc347496
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2021-06-17 13:58:47 +0300

    challages

--- ex3Functions.py
+++ ex3Functions.py
@@ -22,3 +22,21 @@ math1 = do_math(5, 7)
 math2 = do_math(11, 34)
 
 print("First sum is", math1, "and the secound sum is", math2)
+
+# python Write a function
+
+def is_leap(year):
+    leap = False
+
+    if year % 400 == 0:
+        return True
+    if year % 100 == 0:
+        return False
+    if year % 4 == 0:
+        return True
+    else:
+        return False
+
+    return leap
+
+year = int(input())
--- ex4Conditions.py
+++ ex4Conditions.py
@@ -8,3 +8,31 @@ elif check == "It desnt matter any more"
     print("Something like that")
 else:
     print("In the end its True")
+
+# Python If-Else
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+n = int(input().strip())
+check = {True: "Not Weird", False: "Weird"}
+
+print(check[
+        n%2==0 and (
+            n in range(2,6) or
+            n > 20)
+    ])
+
+# Python Division
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+
+
+print("{0}\n{1}".format(a//b, a/b))
