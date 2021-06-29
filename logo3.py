a15e92be3ca3168f1540cf532ebb1967b8f6c738
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2021-06-29 17:33:25 +0300

    logo with loops

--- logo3.py
+++ logo3.py
@@ -4,83 +4,37 @@ pen = turtle.Turtle()
 
 pen.begin_fill()
 
-pen.left(90)
+# ==========================> loop 1
+
+for i in range(2):
+    pen.left(90)
+    pen.forward(100)
+
+# ==========================> loop 2
+
+for i in range(3):
+    pen.right(30)
+    pen.forward(60)
+    pen.right(120)
+    pen.forward(60)
+    pen.right(30)
+    pen.forward(100)
+    pen.left(90)
+    pen.forward(100)
+
 pen.forward(100)
 
-pen.left(90)
-pen.forward(100)
+# =========================> loop 3
 
-pen.right(30)
-pen.forward(60)
-
-pen.right(120)
-pen.forward(60)
-
-pen.right(30)
-pen.forward(100)
-
-pen.left(90)
-pen.forward(100)
-
-pen.right(30)
-pen.forward(60)
-
-pen.right(120)
-pen.forward(60)
-
-pen.right(30)
-pen.forward(100)
-
-pen.left(90)
-pen.forward(100)
-
-pen.right(30)
-pen.forward(60)
-
-pen.right(120)
-pen.forward(60)
-
-pen.right(30)
-pen.forward(100)
-
-pen.left(90)
-pen.forward(200)
-
-pen.left(90)
-pen.forward(100)
-
-pen.right(30)
-pen.forward(60)
-
-pen.right(120)
-pen.forward(60)
-
-pen.right(30)
-pen.forward(100)
-
-pen.left(90)
-pen.forward(100)
-
-pen.right(30)
-pen.forward(60)
-
-pen.right(120)
-pen.forward(60)
-
-pen.right(30)
-pen.forward(100)
-
-pen.left(90)
-pen.forward(100)
-
-pen.right(30)
-pen.forward(60)
-
-pen.right(120)
-pen.forward(60)
-
-pen.right(30)
-pen.forward(100)
+for i in range(3):
+    pen.left(90)
+    pen.forward(100)
+    pen.right(30)
+    pen.forward(60)
+    pen.right(120)
+    pen.forward(60)
+    pen.right(30)
+    pen.forward(100)
 
 pen.left(90)
 pen.forward(100)
