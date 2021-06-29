3221ac18fd77ada4d60f67fa5c70e4d0492dabf9
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2021-06-29 09:25:21 +0300

    png

---
+++ img.py
@@ -0,0 +1,13 @@
+import png
+
+width = 200
+height = 200
+img = []
+for y in range(height):
+    row = ()
+    for x in range(width):
+        row = row + (x, max(0, 200 - x - y), y)
+    img.append(row)
+with open('gradient3.png', 'wb') as f:
+    w = png.Writer(width, height, greyscale=False)
+    w.write(f, img)
