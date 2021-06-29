80dbeca241524ac2994f439731e9696d78668672
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2021-06-29 17:16:17 +0300

    logo

--- logo2.py
+++ logo2.py
@@ -1,3 +1,17 @@
+import png
 
+s = ['01010101',
+     '01010101',
+     '01010111',
+     '01010101',
+     '01010101',
+     '01110101']
+
+s = [[int(c) for c in row * 50] for row in s * 50]
+
+palette=[(0x00,0x00,0x00), (0x23,0x64,0x42)]
+w = png.Writer(len(s[0]), len(s), palette=palette, bitdepth=8)
+f = open('logo2.png', 'wb')
+w.write(f, s)
 
 
