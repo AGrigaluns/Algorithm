b49a0d0e1990d525f2978e5494fa7ff88ccb7b74
Alvis Grigaluns <alvis.grigaluns@crealoz.fr>
2021-06-30 10:10:43 +0300

    portfolio

---
+++ Portfolio/scripts.js
@@ -0,0 +1,19 @@
+
+document.addEventListener("DOMContentLoaded", function (event) {
+  var acc = document.getElementsByClassName("accordion");
+  var i;
+
+  console.log(acc);
+
+  for (i = 0; i < acc.length; i++) { console.log(i);
+    acc[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      var panel = this.nextElementSibling;
+      if (panel.style.maxHeight) {
+        panel.style.maxHeight = null;
+      } else {
+        panel.style.maxHeight = panel.scrollHeight + "px";
+      }
+    });
+  }
+});
