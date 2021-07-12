98c0c34e33aa2b67086ef085eedb3a4aa50d5e10
Alvis <alvisgrigaluns@gmail.com>
2021-07-12 19:50:25 +0300

    crud

--- api/user_crud/app.py
+++ api/user_crud/app.py
@@ -1,10 +1,8 @@
 from flask import Flask, send_from_directory, jsonify, make_response
 from flask_cors import CORS
-
 import MySQLdb
 import os
 import json
-
 app = Flask(__name__, static_url_path='/')
 cors = CORS(app)
 
@@ -12,10 +10,6 @@ cors = CORS(app)
 @app.route('/static/<path:path>')
 def send_js(path):
     return send_from_directory('./', path)
-
-
-if __name__ == "__main__":
-    app.run()
 
 
 ufile = open('user.txt', 'r')
@@ -26,6 +20,9 @@ ufile.close()
 pfile = open('password.txt', 'r')
 password = pfile.read()[:-1]
 pfile.close()
+
+if __name__ == "__main__":
+    app.run()
 
 
 @app.route("/select")
@@ -52,3 +49,61 @@ def select_by_id(id):
     print("json_data (select):")
     print(json_data)
     return jsonify(data=json_data)
+
+
+@app.route("/get_invoices/<int:id>/<string:year>/<string:month>", methods=['GET'])
+def get_invoices(id, year, month):
+    db = MySQLdb.connect("localhost", user, password, "musicshop")
+    cur = db.cursor()
+    proc = "get_invoices_by_empl"
+    cur.callproc(proc, [id, year, month])
+
+    columns = [x[0] for x in cur.description]
+    data = cur.fetchall()
+    json_data = []
+    for result in data:
+        json_data.append(dict(zip(columns, result)))
+        print("json_data (proc):")
+    print(json_data)
+    response = {'data': json_data}
+    return make_response(jsonify(json_data), 200)
+
+
+@app.route("/create_artist/<int:id>/<string:name>", methods=['POST'])
+def create_artist(id, name):
+    db = MySQLdb.connect("localhost", user, password, "musicshop")
+    cur = db.cursor()
+    query = "INSERT INTO artists VALUES ArtistId=" + str(id), "Name=" + str(name)
+    cur.execute(query)
+    json_data = cur.fetchall()
+    cur.close()
+    print("json_data (creare_artist):")
+    print(json_data)
+    return jsonify(data=json_data)
+
+
+@app.route("/delete_types/<int:id>", methods=['DELETE'])
+def delete_types(id):
+    db = MySQLdb.connect("localhost", user, password, "musicshop")
+    cur = db.cursor()
+    query = "DELETE FROM media_types WHERE MediaTypeId=" + str(id)
+    cur.execute(query)
+    json_data = cur.fetchall()
+    cur.close()
+    print("json_data (delete_types):")
+    print(json_data)
+    return jsonify(data=json_data)
+
+
+@app.route("/add_types/<int:id>/<string:name>", methods=['PUT'])
+def update_genres(id, name):
+    db = MySQLdb.connect("localhost", user, password, "musicshop")
+    cur = db.cursor()
+    query = "UPDATE genres SET Name=" + str(name), "WHERE GenreId=" + str(id)
+    cur.execute(query)
+    json_data = cur.fetchall()
+    cur.close()
+    print("json_data (update_genres):")
+    print(json_data)
+    return jsonify(data=json_data)
+
--- api/user_crud/db_config.py
+++
@@ -1,9 +0,0 @@
-# from app import app
-#from flaskext.mysql import MySQLmysql = MySQL()
-
-# MySQL configurations
-# app.config['MYSQL_DATABASE_USER'] = 'root'
-# app.config['MYSQL_DATABASE_PASSWORD'] = 'Partilife1994!'
-# app.config['MYSQL_DATABASE_DB'] = 'musicshop'
-# app.config['MYSQL_DATABASE_HOST'] = 'localhost'
-# mysql.init_app(app)
